4835ec83ca55b04a4411e9654b3fe0cfb835061f
Dmitry Elfimov <skitehok@mail.ru>
2023-04-08 14:35:49 +0500

    создали массив

--- Рабочий стол/ControlWork/Program.cs
+++ Рабочий стол/ControlWork/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программа, которая из имеющегося массива строк формирует 
+// новый массив из строк, длина которых меньше, либо равна 3 символам.
+
+Console.Write("Введите кол-во элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] FillArray(int size)
+{
+    string[] arr = new string[size];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write("Введите элемент массива: ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+string[] array = FillArray(size);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
